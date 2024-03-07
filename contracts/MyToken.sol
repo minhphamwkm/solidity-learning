f686895fbec5a1b6c5f89cf0616acf9a3a672d3c
minhphamwkm <minhpham@wakumo.vn>
2024-03-07 16:53:47 +0700

    update: mint 1000000 for deployer

---
+++ contracts/MyToken.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract MyToken is ERC20 {
+    constructor() ERC20("MyToken", "MT") {
+        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+    }
+}
