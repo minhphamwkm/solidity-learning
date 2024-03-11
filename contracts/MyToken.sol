b3b32beae40e2c79573d3e705860d7d9e9b7dac2
minhphamwkm <minhpham@wakumo.vn>
2024-03-11 08:55:16 +0700

    update: ex 1

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,12 +1,33 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract MyToken is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {
-        _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+// Deploy được contract trên mạng localhost:
+//  + Contract token ERC20 có tên là "MyToken", symbol là "MTK", decimals là 18
+//  + Tại lúc deploy, mint cho người deploy 1 triệu token (lưu ý là 1 triệu token đã được nhân với 10^18 vì số decimals là 18)
+
+// Có tính năng owner: người sở hữu contract và có các quyền riêng biệt
+// Chỉ owner mới được mint token cho 1 address bất kì với số lượng bất kì
+// Chỉ owner mới được burn token của 1 address bất kì với số lượng bất kì
+
+contract MyToken is ERC20, Ownable {
+    constructor(address owner) Ownable(owner) ERC20("MyToken", "MTK") {
+        _mint(owner, 1000000 * (10 ** uint256(decimals())));
+        // _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+    }
+
+    function burn(address wallet, uint256 amount) public {
+        require(amount > 0, "Amount must be greater than zero");
+        require(balanceOf(wallet) >= amount, "ERC20: burn amount exceeds balance");
+        _burn(wallet, amount);
+    }
+
+    function mint(address wallet, uint256 amount) public Ownable {
+        require(amount > 0, "Amount must be greater than zero");
+        _mint(wallet, amount);
     }
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -11,11 +11,27 @@ async function main() {
   await myToken.symbol(); // MTK
   await myToken.balanceOf(owner.address); // 1,000,000
 
+  // Viết script js/ts để mint token cho 1 address bất kì với số lượng bất kì
   await myToken.mint(addresses[18].address, 1000000);
   await myToken.balanceOf(addresses[18].address); // 1,000,000
 
-  await myToken.transfer(addresses[18].address, 100);
-  await myToken.balanceOf(addresses[18].address); // 100
+  // Viết script js/ts để transfer token từ ví A sang ví B 100 Token
+  await myToken.transferFrom(addresses[18].address, addresses[17].address, 50);
+
+  // Viết scripts transferFrom từ ví A (addresses[18]) sang vì B (addresses[17]) 50 token
+  await myToken.transferFrom(addresses[18].address, addresses[17].address, 50);
+  await myToken.balanceOf(addresses[17].address); // 50
+
+  // Viết script check allowance giữa ví A và ví B sau khi approve
+  await myToken.allowance(addresses[18].address, addresses[17].address); // 50
+
+  // Viết script transferFrom từ ví A sang ví B 50 token, nhưng sender là ví B  (*)
+
+  // Ví A tự burn 10 token từ ví của chính mình
+  await myToken.burn(addresses[17].address, 10);
+  await myToken.balanceOf(addresses[17].address); // 40
+
+  //
 }
 
 // We recommend this pattern to be able to use async/await everywhere
