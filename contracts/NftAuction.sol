d064e0e20146f7a47de6ff9d230209f436972e94
minhphamwkm <minhpham@wakumo.vn>
2024-03-13 14:37:01 +0700

    complete: ex 2: NftToken

--- contracts/NftAuction.sol
+++ contracts/NftAuction.sol
@@ -1,67 +1,67 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.24;
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "./NftToken.sol";
+// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
+// import "./NftToken.sol";
 
 
-contract NftAuction is ERC721 {
-    struct AuctionInfo {
-        address owner;
-        uint256 highestBid;
-        address payable highestBidder;
-        uint256 auctionStartBlock;
-        uint256 auctionEndBlock;
-        bool isEnded;
-        bool isClaimed;
-    }
+// contract NftAuction is ERC721 {
+//     struct AuctionInfo {
+//         address owner;
+//         uint256 highestBid;
+//         address payable highestBidder;
+//         uint256 auctionStartBlock;
+//         uint256 auctionEndBlock;
+//         bool isEnded;
+//         bool isClaimed;
+//     }
 
-    mapping(address => AuctionInfo) auctions;
+//     mapping(address => AuctionInfo) auctions;
 
-    constructor() ERC721("NftAuction", "NFTA") {}
+//     constructor() ERC721("NftAuction", "NFTA") {}
 
-    modifier duringAuction(address _nftId) {
-        require(auctions[_nftId].auctionStartBlock < block.number && block.number < auctions[_nftId].auctionEndBlock, "Auction not open");
-        _;
-    }
+//     modifier duringAuction(address _nftId) {
+//         require(auctions[_nftId].auctionStartBlock < block.number && block.number < auctions[_nftId].auctionEndBlock, "Auction not open");
+//         _;
+//     }
 
-    modifier notDuringAution(address _nftId) {
-        require(auctions[_nftId].auctionStartBlock > block.number || block.number > auctions[_nftId].auctionEndBlock, "Auction not closed");
-        _;
-    }
+//     modifier notDuringAution(address _nftId) {
+//         require(auctions[_nftId].auctionStartBlock > block.number || block.number > auctions[_nftId].auctionEndBlock, "Auction not closed");
+//         _;
+//     }
 
-    modifier isValidNft(address _nftId) {
-        require(abi.encodePacked(auctions[_nftId]).length > 0, "Not owner");
-        _;
-    }
+//     modifier isValidNft(address _nftId) {
+//         require(abi.encodePacked(auctions[_nftId]).length > 0, "Not owner");
+//         _;
+//     }
 
-    modifier isOwner(address _nftId) {
-        require(msg.sender == auctions[_nftId].owner, "Not owner");
-        _;
-    }
+//     modifier isOwner(address _nftId) {
+//         require(msg.sender == auctions[_nftId].owner, "Not owner");
+//         _;
+//     }
 
-    modifier isClaimed(address _nftId) {
-        require(auctions[_nftId].isClaimed, "NFT not claimed");
-        _;
-    }
+//     modifier isClaimed(address _nftId) {
+//         require(auctions[_nftId].isClaimed, "NFT not claimed");
+//         _;
+//     }
 
-    //
-    function addNft(address _nftId, address _owner, uint256 _amount) external {
-        auctions[_nftId] = AuctionInfo(_owner, _amount, payable(address(0)), block.number, block.number + 10, false, false);
-    }
+//     //
+//     function addNft(address _nftId, address _owner, uint256 _amount) external {
+//         auctions[_nftId] = AuctionInfo(_owner, _amount, payable(address(0)), block.number, block.number + 10, false, false);
+//     }
 
-    function bid(address _nftId, uint _amount) payable external duringAuction(_nftId) {
-        require(msg.value > _amount, "Insufficient balance");
-        require(auctions[_nftId].highestBid < _amount, "Bid must be higher than the highest bid");
+//     function bid(address _nftId, uint _amount) payable external duringAuction(_nftId) {
+//         require(msg.value > _amount, "Insufficient balance");
+//         require(auctions[_nftId].highestBid < _amount, "Bid must be higher than the highest bid");
 
-        auctions[_nftId].highestBid = _amount;
-        auctions[_nftId].highestBidder = payable msg.sender;
-    }
+//         auctions[_nftId].highestBid = _amount;
+//         auctions[_nftId].highestBidder = payable msg.sender;
+//     }
 
-    function claim(address _nftId) external notDuringAution(_nftId) {
-    }
+//     function claim(address _nftId) external notDuringAution(_nftId) {
+//     }
 
-    function withdraw(address _nftId) external notDuringAution(_nftId) isOwner(_nftId) {
-    }
-}
+//     function withdraw(address _nftId) external notDuringAution(_nftId) isOwner(_nftId) {
+//     }
+// }
--- contracts/NftToken.sol
+++ contracts/NftToken.sol
@@ -2,19 +2,77 @@
 pragma solidity ^0.8.24;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NftToken is ERC721, Ownable {
-    address owner;
+/**
+ *
+ * Bài 2: Viết contract mua NFT
+ * Contract NFT 721
+ * Trong vòng 10 block, người dùng có quyền đấu giá mua NFT
+ * Người dùng đấu giá bằng ETH
+ * Ở block thứ 10, người dùng có giá trị đấu giá cao nhất sẽ được quyền claim NFT về ví mình
+ * Nếu không có người dùng nào đấu giá, NFT sẽ được trả về cho owner.
+ * Vòng tiếp theo sẽ được bắt đầu sau khi người trước claim NFT hoặc NFT được trả về cho owner.
+ * Thời hạn bắt đầu tính từ block thứ 0 cho đấu giá là lúc contract được deploy.
+ */
+
+contract NftToken is ERC721 {
     string name;
     string symbol;
+
+    address owner;
+
+    address highestBidder;
+    uint256 highestBidAmount;
+
+    uint256 blockStart;
+    uint256 constant TIMELINE = 100;
+
+    uint256 currentTokenId;
+
     constructor(
         address _owner,
         string memory _name,
         string memory _symbol
-    ) ERC721(_name, _symbol) Ownable(_owner) {
+    ) ERC721(_name, _symbol) {
         owner = _owner;
         name = _name;
         symbol = _symbol;
+    }
+
+    event NewBid(address indexed bidder, uint256 indexed tokenId, uint256 amount);
+    event NewNft(uint256 indexed tokenId);
+
+    modifier bidable() {
+        require(blockStart < block.number && blockStart + TIMELINE > block.number, "Auction not open");
+        require(highestBidAmount < msg.value, "Bid must be higher than the highest bid");
+        _;
+    }
+
+    modifier claimable() {
+        require(blockStart > block.number || blockStart + TIMELINE < block.number, "Auction not closed");
+        _;
+    }
+
+    function bid() external payable bidable {
+        if (highestBidder != address(0)) {
+            payable(highestBidder).transfer(highestBidAmount);
+        }
+        highestBidder = payable(msg.sender);
+        highestBidAmount = msg.value;
+
+        emit NewBid(highestBidder, currentTokenId, highestBidAmount);
+    }
+
+    function claim() external claimable {
+        if (highestBidder == address(0)) {
+            _mint(owner, currentTokenId);
+        } else {
+            _mint(highestBidder, currentTokenId);
+        }
+        highestBidder = address(0);
+        highestBidAmount = 0;
+        currentTokenId += 1;
+
+        emit NewNft(currentTokenId);
     }
 }
