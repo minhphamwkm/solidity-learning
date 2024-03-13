24c81639bf498b17ffa12ac624efd2ab626058aa
minhphamwkm <minhpham@wakumo.vn>
2024-03-13 17:02:05 +0700

    update: testcase for MyToken contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,55 +1,12 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const [owner, ...addresses]  = await ethers.getSigners();
+  const [owner, ...addresses] = await ethers.getSigners();
+  const MyToken = await ethers.getContractFactory("MyToken");
+  const NftToken = await ethers.getContractFactory("NftToken");
 
-  const myToken = await ethers.deployContract("MyToken", [owner.address], owner);
-
-  const alice = addresses[0];
-  const bob = addresses[1];
-
-  await myToken.waitForDeployment();
-  await myToken.name(); // MyToken
-  await myToken.symbol(); // MTK
-  await myToken.balanceOf(owner.address); // 1,000,000
-
-  // Viết script js/ts để mint token cho 1 address bất kì với số lượng bất kì
-  let parsedMintAmount = ethers.parseUnits('1000000', 18);
-  await myToken.mint(alice, parsedMintAmount);
-  await myToken.balanceOf(alice); // 1,000,000
-
-  // Viết script js/ts để transfer token từ ví A sang ví B 100 Token
-  let parsedTransferAmount = ethers.parseUnits('100', 18);
-  await myToken.connect(alice).transfer(bob, parsedTransferAmount);
-  await myToken.balanceOf(alice); //999,900
-  await myToken.balanceOf(bob); // 100
-
-  // Viết script approve từ ví A cho ví B 50 Token
-  let parsedApproveAmount = ethers.parseUnits('50', 18);
-  await myToken.connect(alice).approve(bob, parsedApproveAmount);
-  // Viết script check allowance giữa ví A và ví B sau khi approve
-  await myToken.allowance(alice, bob); // 50
-
-  // Viết scripts transferFrom từ ví A sang vì B 50 token
-  let parsedTransferFromAmount = ethers.parseUnits('50', 18);
-  await myToken.connect(alice).approve(owner, parsedTransferFromAmount);
-  await myToken.transferFrom(alice, bob, parsedTransferFromAmount);
-  await myToken.balanceOf(alice); // 999,850
-  await myToken.balanceOf(bob); // 150
-
-  // Viết script transferFrom từ ví A sang ví B 50 token, nhưng sender là ví B (*)
-  await myToken.connect(alice).approve(bob, parsedTransferFromAmount);
-  await myToken.connect(bob).transferFrom(alice, bob, parsedTransferFromAmount);
-  await myToken.balanceOf(alice); // 999,800
-  await myToken.balanceOf(bob); // 200
-
-  // Ví A tự burn 10 token từ ví của chính mình
-  let parsedBurnAmount = ethers.parseUnits('10', 18);
-  // await myToken.transferOwnership(alice);
-  // await myToken.connect(alice).burn(alice, parsedBurnAmount);
-
-  await myToken.connect(alice).selfBurn(parsedBurnAmount);
-  await myToken.balanceOf(alice); // 999,700
+  await MyToken.deploy(owner);
+  await NftToken.deploy();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
---
+++ test/MyToken.ts
@@ -0,0 +1,107 @@
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("MyToken", function () {
+  async function deploy() {
+    const [owner, alice, bob, ...addresses] = await ethers.getSigners();
+
+    const MyToken = await ethers.getContractFactory("MyToken");
+    const myToken = await MyToken.deploy(owner);
+
+    return { myToken, owner, alice, bob, addresses };
+  }
+
+  it("Should correct contract name, symbol & mint to owner 1M", async function () {
+    const { myToken, owner, alice, bob, addresses } = await loadFixture(deploy);
+
+    expect(await myToken.name()).to.equal("MyToken");
+    expect(await myToken.symbol()).to.equal("MTK");
+
+    const parsedMint = ethers.parseUnits("1000000", 18);
+    expect(await myToken.balanceOf(owner.address)).to.equal(parsedMint);
+  });
+
+  it("Should mint to alice 1M", async function () {
+    const { myToken, owner, alice, bob } = await loadFixture(deploy);
+
+    const parsedMint = ethers.parseUnits("1000000", 18);
+    await myToken.mint(alice, parsedMint);
+    expect(await myToken.balanceOf(alice)).to.equal(parsedMint);
+  });
+
+  it("Should transfer from alice to bob 100", async function () {
+    const { myToken, owner, alice, bob } = await loadFixture(deploy);
+
+    const parsedMint = ethers.parseUnits("1000000", 18);
+    const parsedTransferAmount = ethers.parseUnits("100", 18);
+
+    await myToken.mint(alice, parsedMint);
+    await myToken.connect(alice).transfer(bob, parsedTransferAmount);
+
+    const aliceBalance = ethers.formatEther(await myToken.balanceOf(alice));
+    const bobBalance = ethers.formatEther(await myToken.balanceOf(bob));
+    expect(aliceBalance).to.equal("999900.0");
+    expect(bobBalance).to.equal("100.0");
+  });
+
+  it("Should approve from alice to bob 50", async function () {
+    const { myToken, owner, alice, bob } = await loadFixture(deploy);
+
+    const parsedMint = ethers.parseUnits("1000000", 18);
+
+    await myToken.mint(alice, parsedMint);
+    let parsedApproveAmount = ethers.parseUnits("50", 18);
+    await myToken.connect(alice).approve(bob, parsedApproveAmount);
+
+    let allowanceAmount = ethers.formatEther(
+      await myToken.allowance(alice, bob)
+    );
+    expect(allowanceAmount).to.equal("50.0");
+  });
+
+  it("Should transferFrom alice to bob 50", async function () {
+    const { myToken, owner, alice, bob } = await loadFixture(deploy);
+
+    const parsedMint = ethers.parseUnits("1000000", 18);
+    await myToken.mint(alice, parsedMint);
+
+    let parsedApproveAmount = ethers.parseUnits("50", 18);
+    await myToken.connect(alice).approve(owner, parsedApproveAmount);
+    await myToken.transferFrom(alice, bob, parsedApproveAmount);
+
+    const aliceBalance = ethers.formatEther(await myToken.balanceOf(alice));
+    const bobBalance = ethers.formatEther(await myToken.balanceOf(bob));
+    expect(aliceBalance).to.equal("999950.0");
+    expect(bobBalance).to.equal("50.0");
+  });
+
+  it("Should transferFrom alice to bob 50, but sender is bob", async function () {
+    const { myToken, owner, alice, bob } = await loadFixture(deploy);
+
+    const parsedMint = ethers.parseUnits("1000000", 18);
+    await myToken.mint(alice, parsedMint);
+
+    let parsedApproveAmount = ethers.parseUnits("50", 18);
+    await myToken.connect(alice).approve(bob, parsedApproveAmount);
+    await myToken.connect(bob).transferFrom(alice, bob, parsedApproveAmount);
+
+    const aliceBalance = ethers.formatEther(await myToken.balanceOf(alice));
+    const bobBalance = ethers.formatEther(await myToken.balanceOf(bob));
+    expect(aliceBalance).to.equal("999950.0");
+    expect(bobBalance).to.equal("50.0");
+  });
+
+  it("Should alice burn 10 itself", async function () {
+    const { myToken, owner, alice, bob } = await loadFixture(deploy);
+
+    const parsedMint = ethers.parseUnits("1000000", 18);
+    await myToken.mint(alice, parsedMint);
+
+    let parsedBurnAmount = ethers.parseUnits("10", 18);
+    await myToken.connect(alice).selfBurn(parsedBurnAmount);
+
+    const aliceBalance = ethers.formatEther(await myToken.balanceOf(alice));
+    expect(aliceBalance).to.equal("999990.0");
+  });
+});
